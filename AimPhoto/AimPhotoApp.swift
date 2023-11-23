e67c972ed3b40f425d239f31866156cbb7f5144a
Artem Moiseev <moiseev@physics.msu.ru>
2023-11-23 15:59:39 +0300

    Initial Commit

---
+++ AimPhoto/AimPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AimPhotoApp.swift
+//  AimPhoto
+//
+//  Created by Artem on 23.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct AimPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
